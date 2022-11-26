d77d5448027891f97a8a931b4983424b97b078a9
Deepak Keshri <deepakeshari6387@gmail.com>
2022-11-26 11:58:30 +0530

    add edit and view on post

--- server/controller/BlogpostController.js
+++ server/controller/BlogpostController.js
@@ -60,7 +60,7 @@ const deletePost = (req, res) => {
 const showAllPost = (req, res) => {
     const userId = 1;
 
-    Blog.findAll({where : {userId}}).then((data) => {
+    Blog.findAll({ where: { userId } }).then((data) => {
         if (data) {
             return res.status(200).json({
                 msg: "Data Found",
@@ -72,5 +72,37 @@ const showAllPost = (req, res) => {
         res.status(403).json({ err });
     })
 }
+const getPost = (req, res) => {
+    const userId = 1;
+    const id = req.body.id;
 
-module.exports = { addPost, allPost, deletePost, showAllPost }
+    Blog.findOne({ where: { id: id } }).then((data) => {
+        if (data) {
+            return res.status(200).json({
+                msg: "Data Found",
+                data: data,
+                success: true
+            })
+        }
+    }).catch((err) => {
+        res.status(403).json({ err })
+    })
+}
+
+const updatePost = (req, res) => {
+    const id = req.body.id;
+    console.log(id);
+
+    Blog.update({ where: { id } }).then((data) => {
+        if (data) {
+            return res.status(200).json({
+                msg: "Data Updated",
+                data: data,
+                success: true
+            })
+        }
+    }).catch((err) => {
+        res.status(403).json({ err });//console.log(err);
+    })
+}
+module.exports = { addPost, allPost, deletePost, showAllPost, getPost, updatePost }
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const {addPost, allPost, deletePost, showAllPost} = require("./../controller/BlogpostController.js");
+const {addPost, allPost, deletePost, showAllPost, getPost, updatePost} = require("./../controller/BlogpostController.js");
 
 router.post("/addpost", addPost);
 // router.get("/addpost", editPost);
 router.get("/allpost", allPost);
 router.post("/delete",deletePost);
 router.get("/getAllPost",showAllPost)
+router.post("/getpost", getPost);
+router.post("/updatepost", updatePost);
 
 module.exports = router;
